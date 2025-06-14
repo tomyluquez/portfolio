c7628d2572e17cb7af3aec94fc5842767ed28dc8
Tomas luquez <luquez1431@gmail.com>
2025-06-13 23:36:19 -0300

    cv nuevo

--- src/components/About/about.tsx
+++ src/components/About/about.tsx
@@ -7,19 +7,17 @@ export const About = () => {
       <div className="flex flex-col gap-2">
         <p className="text-5xl font-bold">Hola, soy Tomas </p>
         <span className="text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-6xl font-bold">
-          <h1>desarrollador frontend</h1>
+          <h1>desarrollador FullStack</h1>
         </span>
       </div>
       <p className="dark:text-slate-500 text-palette-900 leading-relaxed">
-        Desarrollador web frontend con sólida experiencia en HTML, CSS,
-        JavaScript, TypeScript, React y Angular. Especializado en la creación de
-        interfaces web atractivas, funcionales y optimizadas, con un enfoque en
-        la usabilidad y el rendimiento. Complemento mi experiencia con
-        conocimientos en desarrollo backend, utilizando Node.js, Express y
-        Sequelize para construir aplicaciones robustas y escalables. Además,
-        poseo experiencia en el manejo de API REST, incluyendo desarrollo con C#
-        y .NET. Estoy comprometido con el aprendizaje continuo y en busca de
-        nuevas oportunidades para aportar y crecer profesionalmente.
+        Desarrollador Web Fullstack con sólida experiencia construyendo
+        aplicaciones modernas y escalables. Mis conocimientos principales en el
+        frontend son React y Angular, y para backend Node.js, Express y C#.
+        Experiencia trabajando con bases de datos relacionales y no relacionales
+        (MySQL y MongoDB), APIs REST y metodologías ágiles como SCRUM. Busco un
+        entorno desafiante donde pueda seguir creciendo, aportar valor y
+        consolidar mis conocimientos técnicos.
       </p>
       <div className="flex flex-wrap gap-4 items-center justify-center">
         <a className="primary-button" href="mailto:luquez1431@gmail.com">
@@ -27,8 +25,8 @@ export const About = () => {
         </a>
         <a
           className="primary-button"
-          href="/CV_Tomas_Luquez.docx"
-          download="CV_Tomas_Luquez.docx"
+          href="/CV_Tomas_E_Luquez.pdf"
+          download="CV_Tomas_E_Luquez.pdf"
         >
           Descargar CV
         </a>
